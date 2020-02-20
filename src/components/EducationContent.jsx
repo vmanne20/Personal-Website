0af7f6f32453bb7417652fe81779589d4be8372f
Vamsi Manne <vmanne20@Vamsis-MBP.fios-router.home>
2020-02-20 01:40:29 -0500

    Create Education Page

--- src/components/EducationContent.jsx
+++ src/components/EducationContent.jsx
@@ -7,20 +7,21 @@ class EducationContent extends Component {
     render() {
         const tj = {
             link: 'https://tjhsst.fcps.edu/',
-            image: require('../../static/images/canvas.png'),
+            image: require('../../static/images/tjhsst.jpeg'),
             school: 'Thomas Jefferson High School for Science and Technology',
             location: 'Alexandria, VA',
             date: '2012-2016',
-            description: 'Advanced Studies Diploma, Focus in Computer Science and Neuroscience'
+            description: [<br/>, 'Advanced Studies Diploma', <br/>, 'Focus in Computer Science and Neuroscience']
         };
 
         const vt = {
             link: 'https://vt.edu/',
-            image: require('../../static/images/canvas.png'),
+            image: require('../../static/images/vt.jpg'),
             school: 'Virginia Tech',
             location: 'Blacksburg, VA',
             date: '2016-2019',
-            description: 'Bachelors in Computer Science, Summa Cum Laude'
+            description: [<br/>, 'B.S. Computer Science', <br/> ,'Summa Cum Laude', <br/>, <br/>, '\
+            ', <b>Activities:</b>, ' Galileo Engineering Living/Learning Community, Hackers@VT, ACM ICPC Programming Team']
         };
 
         return(
