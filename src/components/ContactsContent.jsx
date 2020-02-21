f75bd42ead1588cc4a1ef574fac2aaa2800ecff2
Vamsi Manne <vmanne20@Vamsis-MBP.fios-router.home>
2020-02-21 11:30:00 -0500

    Add styling and hrefs to social media icons

--- src/components/ContactsContent.jsx
+++ src/components/ContactsContent.jsx
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React from 'react';
 import '../css/index.css';
-import { Grid, Card, Avatar, Link, IconButton } from '@material-ui/core';
+import { Grid, Avatar, Link, IconButton, Tooltip, Zoom } from '@material-ui/core';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import EmailIcon from '@material-ui/icons/Email';
@@ -23,56 +23,67 @@ const styles = theme => ({
         height: 70,
         width: 70,
         backgroundColor: "white",
-        // border: 1,
-        // borderColor: 'text.primary',
-        // borderRadius: "50%",
+        overflow: "hidden",
+        transition: "transform .3s ease",
+        '&:hover': {
+            filter: "brightness(50%)",
+            transform: "scale(1.15)"
+        }
     },
     icon: {
-        fill: "rgba(49, 42, 52, 0.845)"
-        // fill: "black"
+        // fill: "black",
+        fill: "rgba(43, 41, 62, 0.845)"
     }
 });
 
 const ContactsContent = withStyles(styles)(({ classes }) => {
     return(
         <div className="ContactsContent">
-            <Grid container spacing={1} direction="row" justify="center" alignItems="center">
+            <Grid container spacing={0} direction="row" justify="center" alignItems="center">
                 <Grid item>
                     <Link href="https://www.linkedin.com" target="_blank">
                         <IconButton>
-                            <Avatar className={classes.avatar}>
-                                <DescriptionIcon className={classes.icon} fontSize="large"/>
-                            </Avatar>
+                            <Tooltip title="View Resume" TransitionComponent={Zoom} arrow>
+                                <Avatar className={classes.avatar}>
+                                    <DescriptionIcon className={classes.icon} fontSize="large"/>
+                                </Avatar>
+                            </Tooltip>
+                        </IconButton>
+                    </Link>   
+                </Grid>
+
+                <Grid item>
+                    <Link href="https://www.linkedin.com/in/vamsimanne6" target="_blank">
+                        <IconButton>
+                            <Tooltip title="View LinkedIn Profile" TransitionComponent={Zoom} arrow>
+                                <Avatar className={classes.avatar}>
+                                    <LinkedInIcon className={classes.icon} fontSize="large"/>
+                                </Avatar>
+                            </Tooltip>
                         </IconButton>
                     </Link>
                 </Grid>
 
                 <Grid item>
-                    <Link href="https://www.linkedin.com" target="_blank">
+                    <Link href="https://www.github.com/vmanne20" target="_blank">
                         <IconButton>
-                            <Avatar className={classes.avatar}>
-                                <LinkedInIcon className={classes.icon} fontSize="large"/>
-                            </Avatar>
+                            <Tooltip title="View Github Profile" TransitionComponent={Zoom} arrow>
+                                <Avatar className={classes.avatar}>
+                                    <GitHubIcon className={classes.icon} fontSize="large"/>
+                                </Avatar>
+                            </Tooltip>
                         </IconButton>
                     </Link>
                 </Grid>
 
                 <Grid item>
-                    <Link href="https://www.linkedin.com" target="_blank">
+                    <Link href={"mailto:vmanne20@vt.edu"} target="_blank">
                         <IconButton>
-                            <Avatar className={classes.avatar}>
-                                <GitHubIcon className={classes.icon} fontSize="large"/>
-                            </Avatar>
-                        </IconButton>
-                    </Link>
-                </Grid>
-
-                <Grid item>
-                    <Link href="https://www.linkedin.com" target="_blank">
-                        <IconButton>
-                            <Avatar className={classes.avatar}>
-                                <EmailIcon className={classes.icon} fontSize="large"/>
-                            </Avatar>
+                            <Tooltip title="Email: vmanne20@vt.edu" TransitionComponent={Zoom} arrow>
+                                <Avatar className={classes.avatar}>
+                                    <EmailIcon className={classes.icon} fontSize="large"/>
+                                </Avatar>
+                            </Tooltip>
                         </IconButton>
                     </Link>
                 </Grid>
